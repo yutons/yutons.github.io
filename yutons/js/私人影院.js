f81bc5beea3c406b0c447da43e1ae840e337da7b
姚建康 <yuton.yao@qq.com>
2024-08-02 16:49:04 +0800

    1

---
+++ yutons/js/私人影院.js
@@ -0,0 +1,99 @@
+var rule = {
+    title: '私人影院',
+    host: 'http://47.94.19.95',
+    homeUrl: '/api.php/provide/vod?ac=detail',
+    searchUrl: '/api.php/provide/vod?ac=detail&wd=**&pg=fypage',
+    detailUrl: '/api.php/provide/vod?ac=detail&ids=fyid', //非必填,二级详情拼接链接
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 1,
+    multi: 1,
+    timeout: 5000,
+    limit: 6,
+    url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
+    // -----------------------------------
+    class_name: '少儿&电视剧&电影&综艺&动漫',
+    class_url: '20&21&22&23&24',
+    // -----------------------------------
+    play_parse: true,
+    lazy:`js:
+        try {
+          function getvideo(url) {let jData = JSON.parse(request('./alist?url='+url, {}));if (jData.code == 200) {return jData.data.url} else {return "";}}
+          if (/aliyun/.test(input)) {
+			let videoUrl = getvideo(input);
+			input = {jx: 0,url: videoUrl,parse: 0}
+		  }else{
+			input = {jx: 0,url: videoUrl,parse: 0}
+		  }
+		  
+        } catch (e) {log(e.toString())}
+    `,
+    推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    一级: `js:
+        function getParam(url,name) {
+            var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+            var r = url.split('?')[1].match(reg); //获取url中"?"符后的字符串并正则匹配
+            var context = "";
+            if (r != null)
+                context = decodeURIComponent(r[2]);
+            reg = null;
+            r = null;
+            return context == null || context == "" || context == "undefined" ? "" : context;
+        }
+        let d = [];
+        // 忽略分类
+        let cate_exclude = '999';
+        let type_id = getParam(input,'t');
+        if(!cate_exclude.match(type_id)){
+            let html = request(input);
+            let list = JSON.parse(html).list;
+            list.forEach(function (it){
+                if(!cate_exclude.match(it.type_id)){
+                    d.push({
+                        title:it.vod_name,
+                        img:it.vod_pic,
+                        desc:it.vod_remarks,
+                        url:it.vod_id
+                    });
+                }
+            });
+        }
+        setResult(d);
+        // log(input);
+    `,
+    /**
+     * 资源采集站，二级链接解析
+     */
+    // 二级: `json:list;vod_name;vod_pic;vod_remarks;vod_id`,
+    二级: `js:
+        let html = request(input);
+        let list = JSON.parse(html).list;
+        if(list.length===1){
+           VOD = list[0];
+            VOD.vod_blurb = VOD.vod_blurb.replace(/　/g, '').replace(/<[^>]*>/g, '');
+            VOD.vod_content = VOD.vod_content.replace(/　/g, '').replace(/<[^>]*>/g, '');
+        }
+    `,
+    /**
+     * 搜索解析 过滤部分资源
+     */
+    // 搜索: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
+    搜索: `js:
+        let d = [];
+        // 忽略分类
+        let cate_exclude = '999';
+        let html = request(input);
+        let list = JSON.parse(html).list;
+        list.forEach(function (it){
+            if(!cate_exclude.match(it.type_id)){
+                d.push({
+                    title:it.vod_name,
+                    img:it.vod_pic,
+                    desc:it.vod_remarks,
+                    url:it.vod_id
+                });
+            }
+        });
+        setResult(d);
+    `,
+}
